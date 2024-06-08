2e7e18fa30d64206b1629ed5d04b3f35db38036d
Romulo Vieira <romulo.vieira777@gmail.com>
2024-06-08 17:09:21 -0300

    Instalando os Dados de Exemplo

---
+++ Secao_04_Instalando_os_Dados_de_Exemplo/SQLCode/CreateDatabse.sql
@@ -0,0 +1,14 @@
+USE [master]
+GO
+CREATE DATABASE [AdventureWorksDW2012] ON 
+( FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLSERVER2019\MSSQL\DATA\AdventureWorksDW2012_Data.mdf' )
+ FOR ATTACH
+GO
+
+
+USE [master]
+GO
+CREATE DATABASE [AdventureWorks2012] ON 
+( FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLSERVER2019\MSSQL\DATA\AdventureWorks2012_Data.mdf' )
+ FOR ATTACH
+GO
